9905f50f690f7b8d06683f9603c0eee2690f4ce3
itaik1993@gmail.com <itaik1993@gmail.com>
2021-08-03 13:39:30 +0300

    update website

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,12 @@
 module.exports = {
-  mode: "",
+  mode: "jit",
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       fontFamily: {
         roboto: ['"Roboto"'], // Ensure fonts with spaces have " " surrounding it.
+        noto: ['"Noto Sans JP"'], // Ensure fonts with spaces have " " surrounding it.
       },
     },
   },
