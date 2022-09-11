ce5ef953dab9abc754c80a68ab11fcac8dbb3164
Itai Keren <itaik1993@gmail.com>
2022-09-11 17:14:10 +0300

    update website look

--- pages/_document.js
+++ pages/_document.js
@@ -1,34 +1,52 @@
-import Document, { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from "next/document";
 
-class MyDocument extends Document {
-  static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx);
-    return { ...initialProps };
+const modeScript = `
+  let darkModeMediaQuery = window.matchMedia('(prefers-color-scheme: dark)')
+
+  updateMode()
+  darkModeMediaQuery.addEventListener('change', updateModeWithoutTransitions)
+  window.addEventListener('storage', updateModeWithoutTransitions)
+
+  function updateMode() {
+    let isSystemDarkMode = darkModeMediaQuery.matches
+    let isDarkMode = window.localStorage.isDarkMode === 'true' || (!('isDarkMode' in window.localStorage) && isSystemDarkMode)
+
+    if (isDarkMode) {
+      document.documentElement.classList.add('dark')
+    } else {
+      document.documentElement.classList.remove('dark')
+    }
+
+    if (isDarkMode === isSystemDarkMode) {
+      delete window.localStorage.isDarkMode
+    }
   }
 
-  render() {
-    return (
-      <Html>
-        <Head>
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@100;200;300;400;500;600;700"
-            rel="stylesheet"
-          />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900"
-            rel="stylesheet"
-          />
-          <link href="https://fonts.googleapis.com/css2?family=Edu+SA+Beginner:wght@400;500;600;700" rel="stylesheet" />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
+  function disableTransitionsTemporarily() {
+    document.documentElement.classList.add('[&_*]:!transition-none')
+    window.setTimeout(() => {
+      document.documentElement.classList.remove('[&_*]:!transition-none')
+    }, 0)
   }
+
+  function updateModeWithoutTransitions() {
+    disableTransitionsTemporarily()
+    updateMode()
+  }
+`;
+
+export default function Document() {
+  return (
+    <Html className="h-full antialiased" lang="en">
+      <Head>
+        <script dangerouslySetInnerHTML={{ __html: modeScript }} />
+        <link rel="alternate" type="application/rss+xml" href={`${process.env.NEXT_PUBLIC_SITE_URL}/rss/feed.xml`} />
+        <link rel="alternate" type="application/feed+json" href={`${process.env.NEXT_PUBLIC_SITE_URL}/rss/feed.json`} />
+      </Head>
+      <body className="flex flex-col h-full bg-zinc-50 dark:bg-black">
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
 }
-
-export default MyDocument;
--- pages/index.js
+++ pages/index.js
@@ -1,56 +1,52 @@
 import Head from "next/head";
-import Image from "next/image";
-import Button from "../components/button";
+import Link from "next/link";
+import { Container } from "../components/Container";
+import { DribbbleIcon, GitHubIcon, InstagramIcon, LinkedInIcon, TwitterIcon } from "../components/SocialIcons";
+
+function SocialLink({ icon: Icon, ...props }) {
+  return (
+    <Link className="p-1 -m-1 group" {...props}>
+      <Icon className="w-6 h-6 transition cursor-pointer fill-zinc-500 hover:fill-zinc-600 dark:fill-zinc-400 dark:hover:fill-zinc-300" />
+    </Link>
+  );
+}
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className="py-2 mt-32">
       <Head>
-        <title>Itai Keren</title>
+        <title>Itai Keren - Software engineer, product designer, and entrepreneur</title>
+        <meta
+          name="description"
+          content="I'm Itai, a software engineer, product designer and entrepreneur based in Israel."
+        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className="flex flex-col flex-1 w-full px-10 mt-20 space-y-5 md:mt-32 lg:w-3/5 font-inter">
-        <header className="flex flex-col">
-          <div className="text-3xl font-bold">Hey, I'm Itai.</div>
-          <div className="text-xl">
-            A <span className="font-roboto-mono">developer</span> and{" "}
-            <span className="text-2xl font-edu-sa">designer</span> focusing on visual ideas.
+      <Container className="mt-9">
+        <div className="max-w-2xl">
+          <div className="flex flex-col space-y-3">
+            <h1 className="text-4xl text-indigo-600 select-none dark:text-indigo-500 sm:text-5xl">✦</h1>
+            <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
+              Software engineer, product designer, and entrepreneur.
+            </h1>
           </div>
-        </header>
-        <div className="w-full">
-          <div className="space-y-5 font-light">
-            <p>
-              Started programming in high school, worked as a{" "}
-              <span className="font-bold">full stack developer and product designer</span>, and got my BSc degree in{" "}
-              <span className="font-bold">Software Engineering</span> from Afeka Academic College.
-            </p>
-            <p>
-              Big passion for <span className="font-bold">open source</span> projects and always looking for
-              opportunities to contribute. I'm also a big fan of FPS & RTS games, movies and gadgets.
-            </p>
-          </div>
-          <div className="my-5 bg-[url('../images/divider.svg')] w-full h-1.5" />
-          <div className="flex flex-col justify-between w-full space-y-5 lg:flex-row lg:space-y-0">
-            <Button text={"Github"} image={require("../images/github.svg")} href={"https://github.com/itaikeren/"} />
-            <Button
-              text={"Twitter"}
-              image={require("../images/twitter.svg")}
-              href={"https://twitter.com/itai_keren/"}
+          <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
+            I'm Itai, a software engineer, product designer and entrepreneur based in Israel. Big passion for open
+            source projects and always looking for opportunities to contribute. I'm also a big fan of FPS & RTS games,
+            and gadgets.
+          </p>
+          <div className="flex gap-6 mt-6">
+            <SocialLink href="https://twitter.com/itai_keren/" aria-label="Follow on Twitter" icon={TwitterIcon} />
+            <SocialLink href="https://github.com/itaikeren/" aria-label="Follow on GitHub" icon={GitHubIcon} />
+            <SocialLink
+              href="https://www.linkedin.com/in/itaikeren/"
+              aria-label="Follow on LinkedIn"
+              icon={LinkedInIcon}
             />
-            <Button
-              text={"Linkedin"}
-              image={require("../images/linkedin.svg")}
-              href={"https://www.linkedin.com/in/itaikeren/"}
-            />
-            <Button
-              text={"Dribbble"}
-              image={require("../images/dribbble.svg")}
-              href={"https://dribbble.com/itaikeren/"}
-            />
+            <SocialLink href="https://dribbble.com/itaikeren/" aria-label="Follow on Dribbble" icon={DribbbleIcon} />
           </div>
         </div>
-      </main>
+      </Container>
     </div>
   );
 }
